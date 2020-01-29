1a6a61d60c913045a15c0b242131a4e17324b877
Kevin Dota <kevin.dot.kd@gmail.com>
2020-01-29 20:20:47 +0100

    added the server.js but still have to change people

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ bodyParser = require('body-parser');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_DB);
 
+                            //this we use for images style sheets or fonts that will be static
+app.set('view engine', 'pug');
+
+// serve static files from the `public` folder
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -22,6 +28,21 @@ routes = require('./api/routes/enemyRoutes');
 routes(app);
 
 app.listen(port);
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Homepage',
+        people: people.profiles               //this adds the people to the site
+      });
+  });
+
+  app.get('/profile', (req, res) => {           //we use this for the second part of the page we we have detailed info for each person
+    const person = people.profiles.find(p => p.id === req.query.id);
+    res.render('profile', {
+      title: `About ${person.firstname} ${person.lastname}`,
+      person,
+    });
+  });
 
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found i guess'})
