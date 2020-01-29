fba8627c38e4fdbac97932f34029a13d2119a77e
Kevin Dota <kevin.dot.kd@gmail.com>
2020-01-29 23:34:43 +0100

    tried to implement individual enemy screens but did not work but added resizable

--- server.js
+++ server.js
@@ -35,17 +35,26 @@ app.listen(port);
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Homepage',
-        people: people.profiles               //this adds the people to the site
+        enemy: enemy.profiles               //this adds the people to the site
       });
   });
 
   app.get('/profile', (req, res) => {           //we use this for the second part of the page we we have detailed info for each person
-    const person = people.profiles.find(p => p.id === req.query.id);
-    res.render('profile', {
-      title: `About ${person.firstname} ${person.lastname}`,
-      person,
-    });
-  });
+    const enemy = enemy.findOne({_id: req.params.id}, function(err, enemy){
+      if (err) {
+          res.json(err);
+      }
+      else if (!enemy) {
+          res.json("Enemy doesn't exists.");
+      }
+      else{
+        res.render('profile', {
+          title: `About ${enemy.name}`,
+          enemy:enemy.profile
+        });
+      }
+  })});
+    
 
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found i guess'})
